fd16cad2ab3c3d03d95dd4739ebfacb9f67b21e0
DuminduViduranga <idvmadhushan@gmail.com>
2024-04-30 11:15:19 +0530

    Adding WordList.py

---
+++ WordList.py
@@ -0,0 +1,6 @@
+word_list = [
+    "hangman", "computer", "programming", "python", "developer",
+    "coding", "software", "algorithm", "internet", "keyboard",
+    "website", "application", "database", "network", "security",
+    "javascript", "interface", "framework", "variable", "function"
+]
